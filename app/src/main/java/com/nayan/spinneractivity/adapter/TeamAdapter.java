15c172540452a38277a6abe133c0e66b6d109e5a
nayan5565 <nayan5565@gmail.com>
2017-07-22 00:48:26 +0600

    baseadapter and some prob solved

--- app/src/main/java/com/nayan/spinneractivity/adapter/TeamAdapter.java
+++ app/src/main/java/com/nayan/spinneractivity/adapter/TeamAdapter.java
@@ -3,47 +3,38 @@ package com.nayan.spinneractivity.adapter;
 import android.app.Activity;
 import android.content.Context;
 import android.content.res.Resources;
-import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.nayan.spinneractivity.R;
 import com.nayan.spinneractivity.model.MTeam;
 
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by Nayan on 7/21/2017.
  */
 public class TeamAdapter extends ArrayAdapter<MTeam> {
     private Activity activity;
-    private ArrayList data;
+    private ArrayList<MTeam> teamArrayList;
     public Resources res;
-    MTeam tempValues = null;
+    MTeam mTeam;
     LayoutInflater inflater;
 
     /*************
      * CustomAdapter Constructor
      *****************/
-    public TeamAdapter(
-            Activity activitySpinner,
-            int textViewResourceId,
-            ArrayList objects,
-            Resources resLocal
-    ) {
+    public TeamAdapter(Activity activitySpinner, int textViewResourceId, ArrayList<MTeam> objects, Resources resLocal) {
         super(activitySpinner, textViewResourceId, objects);
 
         /********** Take passed values **********/
         activity = activitySpinner;
-        data = objects;
+        teamArrayList = objects;
         res = resLocal;
+        mTeam=new MTeam();
 
         /***********  Layout inflator to call external xml layout () **********************/
         inflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
@@ -60,18 +51,18 @@ public class TeamAdapter extends ArrayAdapter<MTeam> {
         return getCustomView(position, convertView, parent);
     }
 
-    // This funtion called for each row ( Called data.size() times )
+    // This funtion called for each row ( Called teamArrayList.size() times )
     public View getCustomView(int position, View convertView, ViewGroup parent) {
 
         /********** Inflate spinner_rows.xml file for each row ( Defined below ) ************/
         View row = inflater.inflate(R.layout.team_row, parent, false);
 
         /***** Get each Model object from Arraylist ********/
-        tempValues = null;
-        tempValues = (MTeam) data.get(position);
+
+        mTeam = teamArrayList.get(position);
 
         TextView team = (TextView) row.findViewById(R.id.TxtTeam);
-        team.setText(tempValues.getTeam());
+        team.setText(mTeam.getTeam());
 
 //        if (position == 0) {
 //
@@ -79,7 +70,7 @@ public class TeamAdapter extends ArrayAdapter<MTeam> {
 //            team.setText("Please select company");
 //        } else {
 //            // Set values for spinner each row
-//            team.setText(tempValues.getTeam());
+//            team.setText(mTeam.getTeam());
 //
 //        }
 
